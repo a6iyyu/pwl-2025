629eff2301a6ab02a914dbe845c0911f8d2144e8
a6iyyu <mizukinako7@gmail.com>
2025-02-18 10:39:06 +0700

    Learning routes.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/user/{name?}', fn($name='John') => "Nama Saya $name");
+
+Route::get('/articles/{id}', function ($id_article) {
+    return "Halaman Artikel dengan ID $id_article.";
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($id_post, $id_comment) {
+    return "Pos ke-$id_post dan komentar ke-$id_comment";
+});
+
+Route::get('/', fn() => "Selamat Datang");
+Route::get('/hello', fn() => "Hello");
+Route::get('/world', fn() => "World");
+
+Route::get('/about', function () {
+    return "
+        NIM\t\t\t: 2341720115
+        <br />
+        Nama\t\t\t: Rafi Abiyyu Airlangga
+    ";
 });

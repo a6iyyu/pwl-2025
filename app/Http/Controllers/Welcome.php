68fb88b0862d43d70830ebbcc69b77bbdc0282d3
a6iyyu <mizukinako7@gmail.com>
2025-02-18 13:39:37 +0700

    Learning views.

--- app/Http/Controllers/Welcome.php
+++ app/Http/Controllers/Welcome.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class Welcome extends Controller
 {
     public function hello()
     {
         return 'Hello World!';
+    }
+
+    public function greeting()
+    {
+        return view('blog.hello')
+            ->with('name', 'Andi')
+            ->with('occupation', 'Astronaut');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,6 @@
 <?php
 
-use App\Http\Controllers\About;
-use App\Http\Controllers\Article;
-use App\Http\Controllers\Home;
-use App\Http\Controllers\Photo;
 use App\Http\Controllers\Welcome;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('photos', Photo::class)->only(['index', 'show']);
-Route::resource('photos', Photo::class)->except(['create', 'store', 'update', 'destroy']);
-
-Route::get('/', [Home::class, 'index']);
-Route::get('/about', [About::class, 'about']);
-Route::get('/articles/{id}', [Article::class, 'articles']);
-
-Route::get('/hello', [Welcome::class, 'hello']);
-Route::get('/user/{name?}', fn($name='John') => "Nama Saya $name");
-
-Route::get('/posts/{post}/comments/{comment}', function ($id_post, $id_comment) {
-    return "Pos ke-$id_post dan komentar ke-$id_comment";
-});
-
-Route::get('/world', fn() => "World");
+Route::get('/greeting', [Welcome::class, 'greeting']);
